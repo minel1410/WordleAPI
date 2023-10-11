4d2e3bd1b5f0e66d18f4e554303bb9ca725bc6d0
MoofyWoofy <59331821+MoofyWoofy@users.noreply.github.com>
2023-10-11 21:34:00 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+from utils import *
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+word_of_the_day: str = get_word_of_the_day()
+
+
+@app.get("/")
+def read_root():
+    return {"ping": "Pong!"}
+
+
+@app.get("/answer")
+def read_root():
+    return {"word": word_of_the_day}
+
+
+@app.post("/{word}")
+def send_guess_word(word: str):
+    guess_word = word.upper()
+    if guess_word == word_of_the_day:
+        return {"game_won": True}
+
+    # Check if the word in the word list
+    word_list = get_word_list()
+    if not binary_search(word_list, 0, len(word_list) - 1, guess_word.lower()):
+        return {
+            "game_won": False,
+            "result": f"{guess_word} is not in the word list"}
+
+    # Check the word against the answer
+    guess_result = []
+    # Calculate guess_word
+    for c in guess_word:
+        guess_result.append(check_character(c, word_of_the_day))
+    return {
+        "game_won": False,
+        "result": guess_result}
+
+
+"""
+uvicorn main:app --reload --port 5000
+"""
---
+++ utils.py
@@ -0,0 +1,47 @@
+import random
+import datetime
+import os
+
+
+def get_word_of_the_day():
+    with open(os.path.join(os.path.dirname(__file__), "word_list.txt"), "r") as f:
+        words = f.readlines()
+        file_length = len(words)
+        random.seed(int(datetime.datetime.now().strftime("%Y%m%d")))
+        return words[random.randint(1, file_length)].strip().upper()
+
+
+def binary_search(arr, low, high, x):
+    # Check base case
+    if high >= low:
+
+        mid = (high + low) // 2
+
+        # If element is present at the middle itself
+        if arr[mid] == x:
+            return mid
+
+        # If element is smaller than mid, then it can only
+        # be present in left subarray
+        elif arr[mid] > x:
+            return binary_search(arr, low, mid - 1, x)
+
+        # Else the element can only be present in right subarray
+        else:
+            return binary_search(arr, mid + 1, high, x)
+
+    else:
+        # Element is not present in the array
+        return False
+
+
+def get_word_list():
+    with open(os.path.join(os.path.dirname(__file__), "word_list.txt"), "r") as f:
+        return sorted(f.read().split("\n")[1:])
+
+
+def check_character(guess_character, answer):
+    return {
+        "is_char_in_word": guess_character in answer,
+        "is_char_in_correct_position": guess_character == answer
+    }
